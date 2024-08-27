3b3e70db5d1608d2dc976b4437041556d9c79454
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2024-08-27 12:08:24 +0600

    navlinks created for navbar

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 export default function Navbar() {
     return (
         <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap:0 ">
-            <h2 className="text-2xl font-semibold">FoodRecipe</h2>
+            <h2 className="text-2xl font-semibold">
+                <NavLink
+                    to={"/"}
+                    className="text-black hover:text-gray-700 duration-300"
+                >
+                    Home
+                </NavLink></h2>
             <form action="">
                 <input
                     type="text"
@@ -10,6 +18,32 @@ export default function Navbar() {
                     className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
                 />
             </form>
+            <ul className="flex gap-5">
+                <li>
+                    <NavLink
+                        to={"/"}
+                        className="text-black hover:text-gray-700 duration-300"
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to={"/favourites"}
+                        className="text-black hover:text-gray-700 duration-300"
+                    >
+                        Favourites
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to={"/recipe-item/:id"}
+                        className="text-black hover:text-gray-700 duration-300"
+                    >
+                        Details
+                    </NavLink>
+                </li>
+            </ul>
         </nav>
     );
 }
